7d6f5676c2b28ce1b4cc2b7ca9c7045f0442fb5a
Akutagawa <il521li@vip.qq.com>
2022-03-23 20:51:40 +0800

    添加了logback的日志打印，包括打印到文件，但还未指定是哪个文件

--- austin-web/src/main/java/org/mura/austin/AustinApplication.java
+++ austin-web/src/main/java/org/mura/austin/AustinApplication.java
@@ -1,5 +1,7 @@
 package org.mura.austin;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,12 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class AustinApplication {
+    private final Logger logger = LoggerFactory.getLogger(AustinApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(AustinApplication.class, args);
     }
 
     @GetMapping("/hello")
     public String hello(@RequestParam(name = "name", defaultValue = "World") String name) {
+        logger.error("error logback for austin");
+        logger.info("info logback for austin");
+
         return String.format("Hello %s!", name);
     }
 }
