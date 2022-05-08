c56a6102e19fc5b3f6cb69968515e49e14584c41
Akutagawa Murasame <il521li@vip.qq.com>
2022-05-08 12:35:10 +0800

    fix pom.xml

--- austin-web/src/main/java/org/mura/austin/AustinApplication.java
+++ austin-web/src/main/java/org/mura/austin/AustinApplication.java
@@ -10,8 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AustinApplication {
     public static void main(String[] args) {
-//        TODO Apollo的地址，端口是eureka管理界面的端口
-        System.setProperty("apollo.config-service", "http://192.168.159.128:7000");
         SpringApplication.run(AustinApplication.class, args);
     }
 }
